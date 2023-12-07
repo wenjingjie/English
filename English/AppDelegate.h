8eb7283b46dc8af7d1119b027c180456b7cc16e4
wangshengfeng <me@shengfeng.wang>
2023-12-07 21:59:33 +0800

    Initial Commit

---
+++ English/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  English
+//
+//  Created by wangshengfeng on 2023/12/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ English/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  English
+//
+//  Created by wangshengfeng on 2023/12/7.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
