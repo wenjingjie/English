decc16e937d7bbd144a3f59206fd9289fcae241a
wangshengfeng <me@shengfeng.wang>
2023-12-07 22:04:22 +0800

    window

---
+++ English/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  English
+//
+//  Created by wangshengfeng on 2023/12/7.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ English/MainViewController.m
@@ -0,0 +1,21 @@
+//
+//  MainViewController.m
+//  English
+//
+//  Created by wangshengfeng on 2023/12/7.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
